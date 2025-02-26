2bc2a7cbf1ed283af8ccf43d5beea9714d933ef9
wierzbin <wierzbin@C56339L>
2025-02-26 10:09:32 -0500

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("These are the most common data types:\n");
+Console.WriteLine(1 + " - string");
+Console.WriteLine(2 + " - char");
+Console.WriteLine(3 + " - int");
+Console.WriteLine(4 + " - decimal");
+Console.WriteLine(5 + " - bool");
+Console.ReadLine();
