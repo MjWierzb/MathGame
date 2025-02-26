8bed7ac0a8861283db411eda490e7589f0ecd023
Maciej Wierzbinski <maciej.wierzbinski@outlook.com>
2025-02-26 11:10:31 -0500

    Added variables to codebase

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,7 +1,10 @@
 ﻿Console.WriteLine("These are the most common data types:\n");
-Console.WriteLine(1 + " - string");
-Console.WriteLine(2 + " - char");
-Console.WriteLine(3 + " - int");
-Console.WriteLine(4 + " - decimal");
-Console.WriteLine(5 + " - bool");
+Console.WriteLine(1 + " - string, example: " + "Maciej");
+Console.WriteLine(2 + " - char, example: " + "M");
+Console.WriteLine(3 + " - int, example: " + 38);
+Console.WriteLine(4 + " - decimal, example: " + 1.85m);
+Console.WriteLine(5 + " - bool, example: " + false);
 Console.ReadLine();
+
+Console.WriteLine("2" + "2");
+Console.WriteLine(2 + 2);
