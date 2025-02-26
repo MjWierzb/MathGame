c0145d9e8c40cac3b25354ec38a3bdbb2aee9c3c
Maciej Wierzbinski <maciej.wierzbinski@outlook.com>
2025-02-26 11:14:54 -0500

    Variables and Console statements

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,10 +1,18 @@
-﻿Console.WriteLine("These are the most common data types:\n");
+﻿int index = 1;
+string name = "Maciej";
+char initial = 'M' ;
+int age = 38;
+decimal height = 1.85m;
+bool isMarried = true;
+
+Console.WriteLine("These are the most common data types:\n");
 Console.WriteLine(1 + " - string, example: " + "Maciej");
-Console.WriteLine(2 + " - char, example: " + "M");
+Console.WriteLine(2 + " - char, example: " + 'M');
 Console.WriteLine(3 + " - int, example: " + 38);
 Console.WriteLine(4 + " - decimal, example: " + 1.85m);
-Console.WriteLine(5 + " - bool, example: " + false);
-Console.ReadLine();
+Console.WriteLine(5 + " - bool, example: " + true);
 
 Console.WriteLine("2" + "2");
 Console.WriteLine(2 + 2);
+
+Console.ReadLine();
