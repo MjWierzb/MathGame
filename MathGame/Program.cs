3dc4e9a1f4991f5c96852f4bdb030a62b3482aca
Maciej Wierzbinski <maciej.wierzbinski@outlook.com>
2025-02-27 14:00:30 -0500

    added variable for data types statement

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -5,14 +5,13 @@ int age = 38;
 decimal height = 1.85m;
 bool isMarried = true;
 
-Console.WriteLine("These are the most common data types:\n");
+string myParagraph = "These are the most common data types:";
+Console.WriteLine(myParagraph);
+
 Console.WriteLine(1 + " - string, example: " + "Maciej");
 Console.WriteLine(2 + " - char, example: " + 'M');
 Console.WriteLine(3 + " - int, example: " + 38);
 Console.WriteLine(4 + " - decimal, example: " + 1.85m);
 Console.WriteLine(5 + " - bool, example: " + true);
-
-Console.WriteLine("2" + "2");
-Console.WriteLine(2 + 2);
 
 Console.ReadLine();
