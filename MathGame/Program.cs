30d7166a7530f25d063245933fefa1b689ddfd41
Maciej Wierzbinski <maciej.wierzbinski@outlook.com>
2025-02-27 14:05:42 -0500

    erased previous test code started MathGame codebase by defininig namd and date variables

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -1,17 +1,4 @@
-﻿int index = 1;
-string name = "Maciej";
-char initial = 'M' ;
-int age = 38;
-decimal height = 1.85m;
-bool isMarried = true;
+﻿Console.WriteLine("Please type your name:");
 
-string myParagraph = "These are the most common data types:";
-Console.WriteLine(myParagraph);
-
-Console.WriteLine(1 + " - string, example: " + "Maciej");
-Console.WriteLine(2 + " - char, example: " + 'M');
-Console.WriteLine(3 + " - int, example: " + 38);
-Console.WriteLine(4 + " - decimal, example: " + 1.85m);
-Console.WriteLine(5 + " - bool, example: " + true);
-
-Console.ReadLine();
+string name = Console.ReadLine();
+var date = DateTime.UtcNow;
